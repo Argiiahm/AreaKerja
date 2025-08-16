d5e04c1d99f1ea1a75b452c0d99f4c6ee21592da
argiiahm <argiigmg@gmail.com>
2025-08-16 20:53:11 +0700

    slicing landing page awalan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,102 @@
 @extends('layouts.index')
 
 @section('content')
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    {{-- Search landing page --}}
+    <div
+        class="flex items-center mx-5 lg:w-full lg:mx-auto md:w-full md:mx-auto p-2 border border-gray-300 rounded-lg shadow-sm">
+        <div class="flex items-center flex-1 px-2">
+            <i class="ph ph-magnifying-glass text-zinc-900 text-lg mr-2"></i>
+            <input type="text" placeholder="Posisi lowongan, kata kunci, ..."
+                class="w-full outline-none border-none placeholder-gray-500 text-gray-700" />
+        </div>
+
+        <span class="w-px h-6 bg-gray-300"></span>
+
+        <div class="flex items-center flex-1 px-2">
+            <i class="ph ph-map-pin text-zinc-900 text-lg mr-2"></i>
+            <input type="text" placeholder="Kota, provinsi, kode pos..."
+                class="w-full outline-none placeholder-gray-500 border-none text-gray-700" />
+        </div>
+
+        <button class="ml-2 bg-[#fa6601] cursor-pointer text-white font-medium px-4 py-2 rounded-lg">
+            <div class="hidden lg:block md:block">
+                Cari Lowongan Kerja
+            </div>
+            <div class="block lg:hidden md:hidden">
+                Cari
+            </div>
+        </button>
+    </div>
+
+    {{-- End Searching --}}
+
+    <div class="flex justify-center my-10">
+        <p class="text-[#fa6601] font-semibold text-center lg:text-left md:text-left">Lamar Pekerjaan Kamu ~ <span
+                class="font-normal block lg:inline md:inline text-zinc-600">Dengan Waktu
+                Dan Langkah Yang Tepat</span></p>
+    </div>
+
+    {{-- Kategori --}}
+    <div>
+        <p class="font-semibold text-zinc-700 px-2 lg:px-0 md:px-0">KATEGORI PEKERJAAN POPULER</p>
+        <div class="flex gap-3">
+
+            <div class="px-2 lg:px-0 md:px-0 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-4 gap-2 mt-5 flex-1">
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Teknologi</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Pelayanan</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Administrasi</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Pemasaran</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Pendidik</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Customer Service</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Keuangan</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Kasir</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Admin</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Programmer</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Marketing</p>
+                </div>
+                <div class="border border-gray-300 py-2 shadow-sm">
+                    <p class="text-center text-zinc-700 font-semibold">Multimedia</p>
+                </div>
+            </div>
+            <div class="mt-5 flex flex-col pr-2 lg:pr-0 md:pr-0 gap-2 w-32">
+                <div class="border border-gray-300 py-2 flex items-center justify-center gap-1 shadow-sm text-red-600 font-semibold">
+                    <i class="ph ph-fire text-lg"></i>
+                    Full Time
+                </div>
+                <div class="border border-gray-300 py-2 flex items-center justify-center gap-1 shadow-sm text-sky-500 font-semibold">
+                    <i class="ph ph-globe text-lg"></i>
+                    WFO/WFH
+                </div>
+                <div
+                    class="border border-gray-300 py-2 flex items-center justify-center gap-1 shadow-sm text-orange-600 font-semibold">
+                    <i class="ph ph-graduation-cap text-lg"></i>
+                    Graduate
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- End Kategori --}}
+
+    </div>
 @endsection
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,20 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css" />
     @vite('resources/css/app.css')
 </head>
 
 <body>
     <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+            <div class="flex items-center gap-2">
+                <img src="{{ asset('image/logo-areakerja.png') }}" class="w-20 h-20 " alt="Flowbite Logo">
+                <span class="self-center text-2xl font-bold text-[#fa6601]">areakerja.com</span>
+            </div>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
-                    started</button>
+                <div class="hidden lg:flex md:flex items-center gap-8">
+                    <i class="ph-fill text-[#fa6601] ph-bell text-3xl"></i>
+                    <a href=""
+                        class="text-white bg-[#fa6601] px-10 py-2 rounded-lg cursor-pointer">Masuk</a>
+                </div>
                 <button data-collapse-toggle="navbar-sticky" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-sticky" aria-expanded="false">
@@ -35,27 +43,34 @@
                 <ul
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-[#fa6601]" aria-current="page">Home</a>
+                        <a href="#" class="block py-2 px-3 text-[#fa6601] font-semibold"
+                            aria-current="page">Beranda</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        <a href="#" class="block py-2 px-3 text-[#fa6601] font-semibold">Talent Hunter</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        <a href="#" class="block py-2 px-3 text-[#fa6601] font-semibold">Tips Kerja</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        <a href="#" class="block py-2 px-3 text-[#fa6601] font-semibold">Daftar Kandidat</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 px-3 text-[#fa6601] font-semibold">Pasang Lowongan</a>
+                    </li>
+                    <li class="flex lg:hidden md:hidden justify-between items-center mt-4">
+                        <i class="ph-fill text-[#fa6601] ph-bell text-3xl"></i>
+                        <button type="button"
+                            class="text-white bg-[#fa6601] px-10 py-2 rounded-lg cursor-pointer">Masuk</button>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    @yield('content')
+    <section class="container max-w-screen-lg mx-auto pt-40">
+        @yield('content')
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
